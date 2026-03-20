5c76174d95bb531f6c92aa788ee5add889ef1ae2
DannyPy12 <nogales20104@gmail.com>
2026-03-19 19:58:47 -0700

    Agrega workflow, Dockerfile y pruebas

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
-
 class Calculator:
+    def sum(self, a, b):
+        return a + b
 
-    def sum(self, a: int, b: int) -> int:
-        return a + b  
+    def restar(self, a, b):
+        return a - b
 
-    def restar(self, a: int, b: int) -> int:
-        return a - b  
+    def multiply(self, a, b):
+        return a * b
 
-    def multiply(self, a: int, b: int) -> int:
-        return a * b  
-
-    def divide(self, a: int, b: int) -> float:
+    def divide(self, a, b):
         if b == 0:
-            raise ValueError("no se puede divir entre cero")
-        return a / b  
+            raise ValueError("No se puede dividir entre cero")
+        return a / b
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    print("Suma 2 + 2 =", calc.sum(2, 2))
+    print("Resta 5 - 3 =", calc.restar(5, 3))
+    print("Multiplicación 3 * 4 =", calc.multiply(3, 4))
+    print("División 10 / 2 =", calc.divide(10, 2))
